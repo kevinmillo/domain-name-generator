77005f905ebee1f611c5e08aba8d55836d17bb4b
kevinmillo <kbullor@hotmail.com>
2021-11-20 12:51:00 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dot = [".com", ".net", ".us", ".io"];
+for (let a = 0; a < pronoun.length; a++) {
+  const element1 = pronoun[a];
+  for (let b = 0; b < adj.length; b++) {
+    const element2 = adj[b];
+    for (let c = 0; c < noun.length; c++) {
+      const element3 = noun[c];
+      for (let d = 0; d < dot.length; d++) {
+        const element4 = dot[d];
+        console.log(element1 + element2 + element3 + element4);
+      }
+    }
+  }
+}
